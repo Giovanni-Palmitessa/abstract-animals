222064a5bb8777b81d126c4f4f1f130660367b1c
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-25 14:54:49 +0200

    print methods in main

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -7,8 +7,10 @@ public class Main {
         Gatto gatto = new Gatto("Genoveffa");
         Pappagallo pappagallo = new Pappagallo("Kiwi");
 
+        // Array di Animali
         Animale[] animali = {cane, gatto, pappagallo};
 
+        // Ciclo per stampare i metodi
         for (Animale animale : animali) {
             System.out.println("Mi chiamo -> " + animale.getName());
             animale.eat();
