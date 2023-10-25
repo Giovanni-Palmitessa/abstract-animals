efda17ac0ed7cc059f8c9ba5efb14091420b6db9
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-25 15:54:56 +0200

    Add class DVDPlayer and methods paly and stop

---
+++ src/org/lessons/java/creator/DvdPlayer.java
@@ -0,0 +1,14 @@
+package org.lessons.java.creator;
+
+public class DvdPlayer implements Device{
+    //OVERRIDE METODI PALY E STOP
+    @Override
+    public void play() {
+        System.out.println("Sono un Lettore DVD e sono in Play!");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Sono un Lettore DVD e sono in Stop!");
+    }
+}
