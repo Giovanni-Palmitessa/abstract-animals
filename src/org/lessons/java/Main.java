7d324f78c990fd0141d51b222653acbb33d62b10
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-25 14:39:44 +0200

    Add Main Class

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -2,6 +2,9 @@ package org.lessons.java;
 
 public class Main {
     public static void main(String[] args) {
-
+        // istanzio i nuovi oggetti
+        Cane cane = new Cane("GianGiorgio");
+        Gatto gatto = new Gatto("Genoveffa");
+        Pappagallo pappagallo = new Pappagallo("Kiwi");
     }
 }
