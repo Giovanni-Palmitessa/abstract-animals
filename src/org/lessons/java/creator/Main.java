49907760bdfa81de2df8ab2c10d0aa5793434bf7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-25 17:29:12 +0200

    complete bonus exercise

--- src/org/lessons/java/creator/Main.java
+++ src/org/lessons/java/creator/Main.java
@@ -2,6 +2,18 @@ package org.lessons.java.creator;
 
 public class Main {
     public static void main(String[] args) {
+        // istanzio il creator
+        Creator creator = new Creator();
+
+        // creo il primo dispositivo
+        Device device1 = creator.create("Lettore DVD");
+        device1.play();
+        device1.stop();
+
+        // creo il secondo dispositivo
+        Device device2 = creator.create("Playstation");
+        device2.play();
+        device2.stop();
 
     }
 }
