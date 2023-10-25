3c659b422f6082948b03972de55508e37088c0b4
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-25 14:16:00 +0200

    Add attribute name and constructor in class Animale

--- src/org/lessons/java/Animale.java
+++ src/org/lessons/java/Animale.java
@@ -1,4 +1,13 @@
 package org.lessons.java;
 
 public class Animale {
+    // ATTRIBUTI
+    private String name;
+
+    //COSTRUTTORE
+    public Animale(String name) {
+        this.name = name;
+    }
+
+    //GETTER E SETTER
 }
